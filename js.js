2bcf2afbef2c730e39c85ba46fce032fd774d9b4
amirjalalli <87828297+amirjalalli@users.noreply.github.com>
2021-11-27 13:24:37 +0330

    even-OR-odd

---
+++ js.js
@@ -0,0 +1,10 @@
+function zoj(getNumber) {
+    var getPrametr =Number(prompt("entet your number: "))
+    
+    if (getPrametr % 2 == 0){
+        alert("even")
+    }else{
+        alert("odd")
+    }
+}
+zoj()
